55857ea0b5aef777c241aabdb6fbf829f2c06091
Cesare Cocito <cesarecocito@gmail.com>
2019-07-05 16:28:54 +0200

    Update brew.sh
    fix check for brew installed

--- brew.sh
+++ brew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install command-line tools using Homebrew (if not alredy installed)
-if which brew > /dev/null; then
+if ! which brew > /dev/null; then
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 fi
 
