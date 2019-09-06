0b0e3432eedc0acb0b8d47ce1451b1088a578664
Cesare Cocito <cesarecocito@gmail.com>
2019-09-06 16:10:58 +0200

    generate .bash_profile and .bashrc in setup instead of symlink

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ cd $SOURCE_DIR;
 # Update
 git pull origin master;
 
-# Symlink dotfiles and backup old ones if any
+# Backup old dotfiles
 for file in .aliases .bash_profile .bashrc .bash_prompt .exports .functions .gitignore_global .hgignore_global .path; do
 
     # check if file exists and if it's a symlink
@@ -38,51 +38,26 @@ for file in .aliases .bash_profile .bashrc .bash_prompt .exports .functions .git
         echo -e "Moving ${CYAN}${file} ${NONE}in ${SOURCE_DIR}/backup_${TIMESTAMP}"
         mv ${HOME}/$file ${SOURCE_DIR}/backup_${TIMESTAMP}/$file
     fi
+done
 
-	# link new file
+# Symlink new dotfiles
+for file in .aliases .bash_prompt .exports .functions .gitignore_global .hgignore_global .path; do
     echo -e "Creating symlink to ${SOURCE_DIR}/${CYAN}${file}${NONE} in HOME ..."
     ln -sf ${SOURCE_DIR}/$file ${HOME}/$file
 done
 
+# Print in .bash_profile to source them
+echo -e "Append source to .bash_profile"
+SOURCE_STR="\\nfor file in ~/.{variables,path,bash_prompt,exports,aliases,functions,extra,extra_local,\"git-completion\"}; do [ -r \"\$file\" ] && [ -f \"\$file\" ] && source \"\$file\" done \\nunset file\\n\\n"
+command printf "${SOURCE_STR}" >> "${HOME}/.bash_profile"
+
+# Create .bashrc for Linux compatibility
+echo -e "Create .bashrc"
+SOURCE_BASHPROFILE_STR="\\nsource \${HOME}/.bash_profile\\n"
+command printf "${SOURCE_BASHPROFILE_STR}" >> "${HOME}/.bashrc"
 
 # Enable
 source ${HOME}/.bash_profile
-
-# Install Xcode
-# xcode-select --install
-
-# Install PHP
-# echo -e "${LIGHT_BLUE}Installing Apache and PHP..."
-# brew tap homebrew/dupes
-# brew tap homebrew/versions
-# brew tap homebrew/php
-# brew tap homebrew/apache
-
-# sudo apachectl stop
-# sudo launchctl unload -w /System/Library/LaunchDaemons/org.apache.httpd.plist 2>/dev/null
-# brew install httpd24 --with-privileged-ports --with-http2
-
-# sudo cp -v "$(brew list httpd24 | head -1 | sed 's/\(^.*\/httpd24\/[^\/]*\).*/\1/')"/homebrew.mxcl.httpd24.plist /Library/LaunchDaemons
-# sudo chown -v root:wheel /Library/LaunchDaemons/homebrew.mxcl.httpd24.plist
-# sudo chmod -v 644 /Library/LaunchDaemons/homebrew.mxcl.httpd24.plist
-# sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.httpd24.plist
-
-# brew install php56 --with-httpd24
-
-# Install MySql
-# echo -e "${LIGHT_BLUE}Installing MySql..."
-# brew install mysql
-# mysql.server start
-# mysql_secure_installation
-# ln -sfv /usr/local/opt/mysql/*.plist ${HOME}/Library/LaunchAgents 
-# launchctl load ${HOME}/Library/LaunchAgents/homebrew.mxcl.mysql.plist
-
-# Node packages
-# sudo -v
-# npm install -g grunt-cli
-# npm install -g gulp
-# npm install -g bower
-# npm install -g browser-sync
 
 # Git completions
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ${HOME}/.git-completion
